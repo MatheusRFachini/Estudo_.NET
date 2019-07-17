b692455f7b5ecd1f59303a1c35c57324089a08ad
MatheusRFachini <38798904+MatheusRFachini@users.noreply.github.com>
2019-07-17 08:14:03 -0300

    Projeto CRUD

---
+++ Cadastro/Cadastro/CadastroDATA/Comandos.cs
@@ -0,0 +1,14 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace CadastroDATA
+{
+    public class Comandos
+    {
+        SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=Cadastro;Integrated Security=True");
+        SqlCommand cmd;
+        SqlDataAdapter adapt;
+        int ID = 0;
+
+    }
+    }
